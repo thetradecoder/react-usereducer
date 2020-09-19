73bc2a6bc5b02d6efdd0f37ed558ed0fed31ca74
thetradecoder <thetradecoder@gmail.com>
2020-09-19 13:04:18 +0600

    local update

--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -11,13 +11,13 @@ export default function Navbar(){
                 <div className="navbar-header">
                     <a href="/react-usereducer/" className="navbar-brand">tradecoder</a>
                 </div>
-
-                    <button data-target="#main-menu" data-toggle="collapse" className="navbar-toggler"><span className="navbar-toggler-icon"></span></button>
-                        <div id="main-menu" className="collapse navbar-collapse">
-                            <ul className="navbar-nav ml-auto">
-                                <li className="nav-item"><Link to="/react-usereducer/counter" className="nav-link">Counter</Link></li>
-                            </ul>
-                        </div>       
+                    <button data-target="#main-menu" data-toggle="collapse" className="navbar-toggler"><span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div id="main-menu" className="collapse navbar-collapse">
+                        <ul className="navbar-nav ml-auto">
+                            <li className="nav-item"><Link to="/react-usereducer/counter" className="nav-link">Counter</Link></li>
+                        </ul>
+                    </div>       
             </div>
         </nav>
     );
