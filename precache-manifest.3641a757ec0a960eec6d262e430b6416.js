b93d73d23bf6612b050a4b4613732da05f52bdb5
thetradecoder <thetradecoder@gmail.com>
2020-09-19 11:38:08 +0600

    Updates

---
+++ precache-manifest.3641a757ec0a960eec6d262e430b6416.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4fe005ca2af46c09f11460e3c843929",
+    "url": "/react-usereducer/index.html"
+  },
+  {
+    "revision": "00cabc865ba77d600cd6",
+    "url": "/react-usereducer/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "27118227841cdd52042f",
+    "url": "/react-usereducer/static/js/2.29028831.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-usereducer/static/js/2.29028831.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00cabc865ba77d600cd6",
+    "url": "/react-usereducer/static/js/main.787155c6.chunk.js"
+  },
+  {
+    "revision": "4d59bd6ebb141407b3ff",
+    "url": "/react-usereducer/static/js/runtime-main.5fc61e4e.js"
+  }
+]);
